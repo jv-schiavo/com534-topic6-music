b8a2f57de164681991d059ba2af905800e52ce07
joao victor schiavo <jvgs.schiavo@gmail.com>
2025-11-22 15:15:25 +0000

    Modified Single Class and implemented the Music Interface

--- src/Single.kt
+++ src/Single.kt
@@ -1,3 +1,11 @@
-class Single(val aSide: Song,
-             val bSide: Song) {
+class Single(val aSide: Song, val bSide: Song): Music {
+    // Total playing time = Aside + Bside
+    override fun getPlayingTime(): Double {
+        return aSide.playingTime + bSide.playingTime
+    }
+
+    override fun getAllSongs(): List<Song> {
+        return listOf(aSide, bSide)
+    }
+
 }
